85f31a6096d102049959fd4b760d72514b35beae
ATHULKNAIR <athul2639@gmail.com>
2021-10-26 19:57:25 +0530

    hydra logout challenge

--- src/routes/hydraLogout.ts
+++ src/routes/hydraLogout.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { AdminApi, Configuration } from '@oryd/hydra-client'
+import url from 'url'
 import config from '../config'
 
 const hydraAdmin = new AdminApi(
@@ -9,16 +10,30 @@ const hydraAdmin = new AdminApi(
 )
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  const challenge = String(req.body.login_challenge)
+  const query = url.parse(req.url, true).query
+
+  // The challenge is used to fetch information about the logout request from ORY Hydra.
+  const challenge = String(query.logout_challenge)
   if (!challenge) {
-    console.error('Expected consent_challenge to be set')
-    next(new Error('Expected a login challenge to be set but received none.'))
+    next(new Error('Expected a logout challenge to be set but received none.'))
     return
   }
+
   hydraAdmin
-    .acceptLogoutRequest(challenge)
-    .then(async ({ data: body }) => {
-      res.redirect(String(body.redirect_to))
+    .getLogoutRequest(challenge)
+    // This will be called if the HTTP request was successful
+    .then(() => {
+      // Here we have access to e.g. response.subject, response.sid, ...
+     return hydraAdmin
+        .acceptLogoutRequest(challenge)
+        .then(({ data: body }) => {
+          // All we need to do now is to redirect the user back to hydra!
+          res.redirect(String(body.redirect_to))
+        })
+        // This will handle any error that happens when making HTTP calls to hydra
+        .catch(next)
+      // The most secure way to perform a logout request is by asking the user if he/she really want to log out.
     })
+    // This will handle any error that happens when making HTTP calls to hydra
     .catch(next)
 }
