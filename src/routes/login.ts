606287b505f5465539ae8a300435d9293ee77363
Sidharth Ramesh <tornadoalert@gmail.com>
2022-05-09 11:34:09 +0000

    runs on mac now

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -11,7 +11,7 @@ const kratos = new V0alpha2Api(
 // A simple express handler that shows the login screen.
 export default (req: Request, res: Response, next: NextFunction) => {
   const flow = req.query.flow
-  console.log({flow})
+  console.log({ flow })
   // The flow is used to identify the login and registration flow and
   // return data like the csrf_token and so on.
   if (!flow || !isString(flow)) {
@@ -34,18 +34,19 @@ export default (req: Request, res: Response, next: NextFunction) => {
     return
   }
 
-  console.log('cookies in request header',req.header('cookie'))
+  console.log('cookies in request header', req.header('cookie'))
 
   return (
     kratos
       .getSelfServiceLoginFlow(flow, req.header('cookie'))
       .then(({ status, data: flow, ...response }) => {
-        console.log('status from login flow',status)
+        console.log('status from login flow', status)
         if (status !== 200) {
           return Promise.reject(flow)
         }
 
         // Render the data using a view (e.g. Jade Template):
+        console.log({ flow: JSON.stringify(flow.ui) })
         res.render('login', flow)
       })
       // Handle errors using ExpressJS' next functionality:
